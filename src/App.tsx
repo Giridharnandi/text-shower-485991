cebed1b3b38ec836ec1f34521a06e91d3553f5b8
Giridhar <127533776+Giridharnandi@users.noreply.github.com>
2025-05-30 21:08:25 +0530

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from '@/components/layout/Layout';
+import { Home } from '@/pages/Home';
+import { About } from '@/pages/About';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default App;
